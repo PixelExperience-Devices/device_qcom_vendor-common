5158bb50a2d78dc5ae292385109ff4a5d16028b8
Ed Falk <efalk@cyngn.com>
2015-10-07 11:57:52 -0700

    FM: Fix headphone icon
    Original headphones were mis-matched w.r.t. default/selected. This
    change normalizes the icon. Also fixes a couple compiler warnings
    in FMRadio.java
    
    Change-Id: Iee383bc23455562f21c3c431d5b291506e0e3e13

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -705,7 +705,6 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
       MenuItem item;
       boolean radioOn = isFmOn();
       boolean recording = isRecording();
-      boolean mSpeakerPhoneOn = isSpeakerEnabled();
       boolean sleepActive = isSleepTimerActive();
       boolean searchActive = isScanActive() || isSeekActive();
       Log.d(LOGTAG, "onCreateOptionsmenu");
@@ -777,7 +776,6 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
       boolean radioOn = isFmOn();
       boolean recording = isRecording();
       boolean RtPlusSupported = isRtPlusSupported();
-      boolean mSpeakerPhoneOn = isSpeakerEnabled();
       boolean searchActive = isScanActive() || isSeekActive();
 
       item = menu.findItem(MENU_SCAN_START);
@@ -890,7 +888,7 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
    }
 
    private void enableSpeaker() {
-    //This method with toggle Speaker phone based on existing state .
+       //This method with toggle Speaker phone based on existing state .
        boolean bSpeakerPhoneOn = isSpeakerEnabled();
        if(mService != null) {
            try {
@@ -1501,7 +1499,6 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
         new View.OnClickListener() {
           @Override
           public void onClick(View v) {
-             // TODO Auto-generated method stub
              mSpeakerButton.setClickable(false);
              mSpeakerButton.setOnClickListener(null);
              mHandler.removeCallbacks(mEnableRadioTask);
