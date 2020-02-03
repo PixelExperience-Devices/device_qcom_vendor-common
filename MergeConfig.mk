c079d2a3b6be1c51c2d0171aa019208c8545cc65
Mulugeta Engdaw <mengdaw@codeaurora.org>
2020-02-02 20:28:29 -0800

    Add ota merge configs common to targets in vendor-common     - moving ota_merge_configs to vendor-common because it is only used by       targets not QSSI.
    Change-Id: I29dbccff36e411c2e69ddbcacf21c3c1fd53ebec

--- MergeConfig.mk
+++ MergeConfig.mk
@@ -2,23 +2,23 @@
 ifeq ($(ENABLE_AB), true)
     # Handle Case for QSSI-Dynamic Partition
     ifeq ($(BOARD_DYNAMIC_PARTITION_ENABLE), true)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/dynamic_partition/ab/merge_config_system_misc_info_keys)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/dynamic_partition/ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_misc_info_keys)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_item_list)
     else
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_misc_info_keys)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/without_dynamic_partition/ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_misc_info_keys)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_item_list)
     endif
 else
     # Handle Case for QSSI-Dynamic Partition
     ifeq ($(BOARD_DYNAMIC_PARTITION_ENABLE), true)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/dynamic_partition/non_ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_misc_info_keys)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_misc_info_keys)
     else
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/qssi/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_misc_info_keys)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_misc_info_keys)
     endif
 endif
