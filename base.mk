f318898e2376210c7a9e54230305e4399f47654a
Naga Venkata Durga Ashok Mutyala <nmutya@codeaurora.org>
2020-09-25 11:44:22 +0530

    Enable DebugUtils HAL on all targets
    Change-Id: I48d4907675591f3dae7347b8988f18f4eb1b79f7

--- base.mk
+++ base.mk
@@ -826,11 +826,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2-impl \
     vendor.qti.hardware.servicetracker@1.2-service
 
-#debugUtils
-PRODUCT_PACKAGES_DEBUG += \
-     vendor.qti.hardware.debugutils@1.0-impl \
-     vendor.qti.hardware.debugutils@1.0-service
-
 #debugApp FDA
 PRODUCT_PACKAGES += FDA
 PRODUCT_PACKAGES += debug.script.rc
@@ -943,6 +938,11 @@ PRODUCT_PACKAGES += tcmiface
 
 #intialise PRODUCT_PACKAGES_DEBUG list for debug modules
 PRODUCT_PACKAGES_DEBUG := init.qcom.testscripts.sh
+
+#DebugUtils HAL
+PRODUCT_PACKAGES_DEBUG += \
+    vendor.qti.hardware.debugutils@1.0-impl \
+    vendor.qti.hardware.debugutils@1.0-service
 
 #Add init.qcom.test.rc to PRODUCT_PACKAGES_DEBUG list
 PRODUCT_PACKAGES_DEBUG += init.qcom.test.rc
