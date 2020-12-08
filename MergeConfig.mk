b413eea6af81f17916fe52a94a8c62d85da621cc
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-12-08 11:14:38 +0530

    Use PRODUCT_USE_DYNAMIC_PARTITIONS in MergeConfig.mk.
    Change-Id: Ibe405a5e2402a6865b02450cbf4a2c40a453a0a0

--- MergeConfig.mk
+++ MergeConfig.mk
@@ -1,7 +1,7 @@
 # QSSI merge config files
 ifeq ($(ENABLE_AB), true)
     # Handle Case for QSSI-Dynamic Partition
-    ifeq ($(BOARD_DYNAMIC_PARTITION_ENABLE), true)
+    ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_misc_info_keys)
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_item_list)
@@ -12,7 +12,7 @@ ifeq ($(ENABLE_AB), true)
     endif
 else
     # Handle Case for QSSI-Dynamic Partition
-    ifeq ($(BOARD_DYNAMIC_PARTITION_ENABLE), true)
+    ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_item_list)
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_other_item_list)
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_misc_info_keys)
