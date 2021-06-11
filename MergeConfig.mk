5823f7376fdabbc0b5b8eab3db9e71d3db9539de
vivek mehta <mvivek@codeaurora.org>
2021-06-11 11:31:38 -0700

    vsdk: Dist merge configs for droidcore-unbundled.
    Change-Id: I00976bfc99c8a68a39bb44464f41d92b00909f99

--- MergeConfig.mk
+++ MergeConfig.mk
@@ -1,24 +1,25 @@
 # QSSI merge config files
+
 ifeq ($(ENABLE_AB), true)
     # Handle Case for QSSI-Dynamic Partition
     ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
-            $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_misc_info_keys)
-            $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
-            $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_item_list)
+            $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_misc_info_keys)
+            $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
+            $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_item_list)
     else
-        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_misc_info_keys)
-        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_misc_info_keys)
+        $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_item_list)
     endif
 else
     # Handle Case for QSSI-Dynamic Partition
     ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
-            $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_item_list)
-            $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_other_item_list)
-            $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_misc_info_keys)
+            $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_item_list)
+            $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_other_item_list)
+            $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_misc_info_keys)
     else
-        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_misc_info_keys)
+        $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore droidcore-unbundled,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_misc_info_keys)
     endif
 endif
